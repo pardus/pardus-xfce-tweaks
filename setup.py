746ae564a828dae5e7ccdbfc63da768ab14a7091
Fatih Altun <fatih.altun@pardus.org.tr>
2023-06-20 22:03:55 +0300

    Update project structure, bin script and Main

--- setup.py
+++ setup.py
@@ -1,48 +1,76 @@
 #!/usr/bin/env python3
-from setuptools import setup, find_packages
 import os
+import subprocess
 
-APPLICATION_FOLDER="/usr/share/pardus/pardus-xfce-tweaks"
+from setuptools import setup, find_packages
+
+APPLICATION_FOLDER = "/usr/share/pardus/pardus-xfce-tweaks"
+
+
+def create_mo_files():
+    podir = "po"
+    mo = []
+    for po in os.listdir(podir):
+        if po.endswith(".po"):
+            os.makedirs("{}/{}/LC_MESSAGES".format(podir, po.split(".po")[0]), exist_ok=True)
+            mo_file = "{}/{}/LC_MESSAGES/{}".format(podir, po.split(".po")[0], "pardus-xfce-tweaks.mo")
+            msgfmt_cmd = 'msgfmt {} -o {}'.format(podir + "/" + po, mo_file)
+            subprocess.call(msgfmt_cmd, shell=True)
+            mo.append(("/usr/share/locale/" + po.split(".po")[0] + "/LC_MESSAGES",
+                       ["po/" + po.split(".po")[0] + "/LC_MESSAGES/pardus-xfce-tweaks.mo"]))
+    return mo
+
+
+changelog = "debian/changelog"
+if os.path.exists(changelog):
+    head = open(changelog).readline()
+    try:
+        version = head.split("(")[1].split(")")[0]
+    except:
+        print("debian/changelog format is wrong for get version")
+        version = "0.1.0"
+    f = open("src/__version__", "w")
+    f.write(version)
+    f.close()
 
 data_files = [
-    ("/usr/share/applications/", ["tr.org.pardus.xfce-tweaks.desktop"]),
-    ("/usr/share/locale/tr/LC_MESSAGES/", ["translations/tr/LC_MESSAGES/pardus-xfce-tweaks.mo"]),
-    (APPLICATION_FOLDER, ["pardus-xfce-tweaks.svg"]),
-    (f"{APPLICATION_FOLDER}/assets", ["assets/theme-light.png", "assets/theme-dark.png"]),
-    (f"{APPLICATION_FOLDER}/src", ["src/pardus-xfce-tweaks", "src/MainWindow.py"]),
-    (f"{APPLICATION_FOLDER}/src/xfce", [
-        "src/xfce/ApplicationManager.py",
-        "src/xfce/DatetimeManager.py",
-        "src/xfce/FontManager.py",
-        "src/xfce/KeyboardManager.py",
-        "src/xfce/locale_changer.py",
-        "src/xfce/LocaleManager.py",
-        "src/xfce/PanelManager.py",
-        "src/xfce/PowerManager.py",
-        "src/xfce/ScaleManager.py",
-        "src/xfce/ThemeManager.py",
-        "src/xfce/ThunarManager.py",
-        "src/xfce/WallpaperManager.py",
-        "src/xfce/WhiskerManager.py",
-    ]),
-    (f"{APPLICATION_FOLDER}/ui", ["ui/MainWindow.glade"]),
-    ("/usr/bin/", ["pardus-xfce-tweaks"]),
-    ("/usr/share/icons/hicolor/scalable/apps/", ["pardus-xfce-tweaks.svg"]),
-    ("/usr/share/polkit-1/actions/", ["tr.org.pardus.xfce-tweaks.policy"])
-]
+     ("/usr/share/applications/", ["tr.org.pardus.xfce-tweaks.desktop"]),
+     (APPLICATION_FOLDER, ["pardus-xfce-tweaks.svg"]),
+     (f"{APPLICATION_FOLDER}/assets", ["assets/theme-light.png", "assets/theme-dark.png"]),
+     (f"{APPLICATION_FOLDER}/src", ["src/Main.py", "src/MainWindow.py", "src/__version__"]),
+     (f"{APPLICATION_FOLDER}/src/xfce", [
+         "src/xfce/ApplicationManager.py",
+         "src/xfce/DatetimeManager.py",
+         "src/xfce/FontManager.py",
+         "src/xfce/KeyboardManager.py",
+         "src/xfce/locale_changer.py",
+         "src/xfce/LocaleManager.py",
+         "src/xfce/PanelManager.py",
+         "src/xfce/PowerManager.py",
+         "src/xfce/ScaleManager.py",
+         "src/xfce/ThemeManager.py",
+         "src/xfce/ThunarManager.py",
+         "src/xfce/WallpaperManager.py",
+         "src/xfce/WhiskerManager.py",
+     ]),
+     (f"{APPLICATION_FOLDER}/ui", ["ui/MainWindow.glade"]),
+     ("/usr/bin/", ["pardus-xfce-tweaks"]),
+     ("/usr/share/icons/hicolor/scalable/apps/", ["pardus-xfce-tweaks.svg"]),
+     ("/usr/share/polkit-1/actions/", ["tr.org.pardus.xfce-tweaks.policy"])
+    ] + create_mo_files()
 
 setup(
     name="pardus-xfce-tweaks",
-    version="0.2.0",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=["PyGObject"],
     data_files=data_files,
-    author="Pardus Altyapı",
-    author_email="altyapi@pardus.org.tr",
+    author="Pardus Developers",
+    author_email="dev@pardus.org.tr",
     description="Tweak Pardus XFCE settings",
     license="GPLv3",
-    keywords="",
-    url="https://www.pardus.org.tr",
+    keywords="xfce,tweaks",
+    url="https://github.com/pardus/pardus-xfce-tweaks",
 )
 
 # os.symlink(f"{APPLICATION_FOLDER}/src/pardus-xfce-tweaks", "/usr/bin/pardus-xfce-tweaks")
--- src/Main.py
+++ src/Main.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 
 import sys
+
 import gi
+
 gi.require_version('Gtk', '3.0')
-from gi.repository import GLib, Gio, Gtk
+from gi.repository import Gio, Gtk
 
 from MainWindow import MainWindow
 
+
 class Application(Gtk.Application):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, application_id="tr.org.pardus.xfce-tweaks", **kwargs)
+        super().__init__(*args, application_id="tr.org.pardus.xfce-tweaks", flags=Gio.ApplicationFlags.NON_UNIQUE,
+                         **kwargs)
         self.window = None
-    
+
     def do_activate(self):
         self.window = MainWindow(self)
 
 
-if __name__ == "__main__":
-    app = Application()
-    app.run(sys.argv)
+app = Application()
+app.run(sys.argv)
